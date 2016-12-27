ce7bd5fc295c8e166852edf55188f523332e550c
進撃の雯雯老師 <askahu6@gmail.com>
2016-12-27 12:13:26 +0800

    Update baiduapi.js

--- baiduapi.js
+++ baiduapi.js
@@ -13,14 +13,15 @@
 </head>
 <body>
 	<div id="allmap"></div>
-	<p>返回地图上圆形覆盖范围内的检索结果，并展示在地图上</p >
+	<p>返回北京市地图上圆形覆盖范围内的poi类别检索结果，并展示在地图上</p >
 <div id="r-result"></div>
   </body>
 </html>
 <script type="text/javascript">
 	// 百度地图API功能
+	var category = '学校';
 	var map = new BMap.Map("allmap");            // 创建Map实例
-	var mPoint = new BMap.Point(116.404, 39.915);  
+	var mPoint = new BMap.Point(121.420894, 31.223487);  
 	map.enableScrollWheelZoom();
 	map.centerAndZoom(mPoint,15);
 
@@ -38,5 +39,5 @@
 				document.getElementById("r-result").innerHTML = s.join("<br/>");
 			}
 		}});  
-    local.searchNearby('餐馆',mPoint,1000);
+    local.searchNearby(category,mPoint,1000);
 </script>
