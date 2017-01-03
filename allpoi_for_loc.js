63467f6776e5e6786a1fe54911080f1100c6a162
進撃の雯雯老師 <askahu6@gmail.com>
2017-01-03 17:10:30 +0800

    Create allpoi_for_loc.js

---
+++ allpoi_for_loc.js
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+	<style type="text/css">
+		body, html {width: 100%;height: 100%;margin:0;font-family:"微软雅黑";}
+		#allmap{width:100%;height:500px;}
+		p{margin-left:5px; font-size:14px;}
+	</style>
+	<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=CahOhKj71xM6v5LOgGhprnEHgGMowHHt"></script>
+	<title>圆形区域搜索</title>
+</head>
+<body>
+	<div id="allmap" style="display:none"></div>
+	<div id="r-result"></div>
+  </body>
+</html>
+<script type="text/javascript">
+	
+	// 百度地图API功能
+//	var keyword = str;
+	var map = new BMap.Map("allmap");            // 创建Map实例
+	var mPoint = new BMap.Point(104.055164, 30.650996);  
+	map.enableScrollWheelZoom();
+	map.centerAndZoom(mPoint,15);
+	var circle = new BMap.Circle(mPoint,500,{fillColor:"blue", strokeWeight: 1 ,fillOpacity: 0.3, strokeOpacity: 0.3});
+    map.addOverlay(circle);
+	//var fso=new ActiveXObject(Scripting.FileSystemObject); 
+    //var f=fso.createtextfile("C:\\Users\\WenluluSens\\Documents\\causalnetwork\\test.txt",2,true); 
+		
+	function obj(str){
+	    var local =  new BMap.LocalSearch(map, {renderOptions: {map: map, autoViewport: false},
+	    	pageCapacity:100, 
+	    	onSearchComplete: function(results){
+				// 判断状态是否正确
+				if (local.getStatus() == BMAP_STATUS_SUCCESS){
+					var s = [];
+					for (var i = 0; i < results.getCurrentNumPois(); i ++){
+						s.push(str + ", " + results.getPoi(i).title + ", " + results.getPoi(i).address + ", " + 
+							results.getPoi(i).point.lat + ", " + results.getPoi(i).point.lng);
+						console.log(results.getPoi(i));
+					//	f.writeLine(results.getPoi(i).title + ", " + results.getPoi(i).address + ", " + 
+					//		results.getPoi(i).point.lat + ", " + results.getPoi(i).point.lng);
+					//	console.log(results.getPoi(i));
+					}
+					
+				document.getElementById("r-result").innerHTML += s.join("<br/>");
+				//f.close();
+				}
+		}}); 
+	    local.searchNearby(str,mPoint,250);
+
+var arr = ["餐厅","银行"];
+	
+	for (var i = 0; i < arr.length; i++) {
+		console.log(arr[i])
+		obj(arr[i]);
+	}
+	
+</script>
